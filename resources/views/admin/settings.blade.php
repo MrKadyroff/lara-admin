dcd10bb1537d992e01c7ac635fc68319a7e8bbaf
Ibra <ibraim253@gmail.com>
2019-06-04 18:01:59 +0600

    adpanel

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <div id="content">
   <div id="content-header">
-    <div id="breadcrumb"> <a href="{{ url('admin/dashboard') }}" title="Go to Home" class="tip-bottom"> <a href="#" class="current">Settigns</a> </div>
-    <h1>Admin settings</h1>
+    <div id="breadcrumb"><a href="{{ url('admin/dashboard') }}" title="Go to Home" class="tip-bottom"> <a href="#" class="current">Settigns</a> </div>
+    <h1>Настройки</h1>
   </div>
   <div class="container-fluid"><hr>
     <div class="row-fluid">
@@ -11,30 +11,30 @@
         <div class="span12">
           <div class="widget-box">
             <div class="widget-title"> <span class="icon"> <i class="icon-info-sign"></i> </span>
-              <h5>Update Password</h5>
+              <h5>Обновить пароль</h5>
             </div>
             <div class="widget-content nopadding">
               <form class="form-horizontal" method="post" action="#" name="password_validate" id="password_validate" novalidate="novalidate">
                 <div class="control-group">
-                  <label class="control-label">Current Password</label>
+                  <label class="control-label">Текущий пароль</label>
                   <div class="controls">
                     <input type="password" name="current_pwd" id="current_pwd" />
                     <span id="chkPwd"></span>
                   </div>
                   <div class="control-group">
-                  <label class="control-label">New Password</label>
+                  <label class="control-label">Новый пароль</label>
                   <div class="controls">
                     <input type="password" name="new_pwd" id="new_pwd" />
                   </div>
                 </div>
                 <div class="control-group">
-                  <label class="control-label">Confirm password</label>
+                  <label class="control-label">Пароль потверждения</label>
                   <div class="controls">
                     <input type="password" name="confirm_pwd" id="confirm_pwd" />
                   </div>
                 </div>
                 <div class="form-actions">
-                  <input type="submit" value="Update password" class="btn btn-success">
+                  <input type="submit" value="Обновить пароль" class="btn btn-success">
                 </div>
               </form>
             </div>
--- resources/views/layouts/adminLayout/admin_sidebar.blade.php
+++ resources/views/layouts/adminLayout/admin_sidebar.blade.php
@@ -1,11 +1,11 @@
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
   <ul>
-    <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
-    <li> <a href="charts.html"><i class="icon icon-signal"></i> <span>Charts &amp; graphs</span></a> </li>
-    <li> <a href="widgets.html"><i class="icon icon-inbox"></i> <span>Widgets</span></a> </li>
-    <li><a href="tables.html"><i class="icon icon-th"></i> <span>Tables</span></a></li>
-    <li><a href="grid.html"><i class="icon icon-fullscreen"></i> <span>Full width</span></a></li>
-    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Forms</span> <span class="label label-important">3</span></a>
+    <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Панель управления</span></a> </li>
+    <li> <a href="charts.html"><i class="icon icon-signal"></i> <span>Соц. сети</span></a> </li>
+    <li> <a href="widgets.html"><i class="icon icon-inbox"></i> <span>Группы</span></a> </li>
+    <li><a href="tables.html"><i class="icon icon-th"></i> <span>Пользователи</span></a></li>
+    <!-- <li><a href="grid.html"><i class="icon icon-fullscreen"></i> <span>Full width</span></a></li> -->
+    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span></span></a>
       <ul>
         <li><a href="form-common.html">Basic Form</a></li>
         <li><a href="form-validation.html">Form with Validation</a></li>
@@ -14,7 +14,7 @@
     </li>
     <li><a href="buttons.html"><i class="icon icon-tint"></i> <span>Buttons &amp; icons</span></a></li>
     <li><a href="interface.html"><i class="icon icon-pencil"></i> <span>Eelements</span></a></li>
-    <li class="submenu"> <a href="#"><i class="icon icon-file"></i> <span>Addons</span> <span class="label label-important">5</span></a>
+    <li class="submenu"> <a href="#"><i class="icon icon-file"></i> <span>Addons</span></a>
       <ul>
         <li><a href="index2.html">Dashboard2</a></li>
         <li><a href="gallery.html">Gallery</a></li>
@@ -23,7 +23,7 @@
         <li><a href="chat.html">Chat option</a></li>
       </ul>
     </li>
-    <li class="submenu"> <a href="#"><i class="icon icon-info-sign"></i> <span>Error</span> <span class="label label-important">4</span></a>
+    <li class="submenu"> <a href="#"><i class="icon icon-info-sign"></i> <span>Error</span> </a>
       <ul>
         <li><a href="error403.html">Error 403</a></li>
         <li><a href="error404.html">Error 404</a></li>
@@ -31,7 +31,7 @@
         <li><a href="error500.html">Error 500</a></li>
       </ul>
     </li>
-    <li class="content"> <span>Monthly Bandwidth Transfer</span>
+    <!-- <li class="content"> <span>Monthly Bandwidth Transfer</span>
       <div class="progress progress-mini progress-danger active progress-striped">
         <div style="width: 77%;" class="bar"></div>
       </div>
@@ -44,6 +44,6 @@
       </div>
       <span class="percent">87%</span>
       <div class="stat">604.44 / 4000 MB</div>
-    </li>
+    </li> -->
   </ul>
 </div>
